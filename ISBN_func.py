ebc228cb67b7893ee5f9c1d0c12a03378a74729d
Michael Atkinson <mikeatk88@gmail.com>
2015-01-26 16:30:26 +0900

    Clean up ISBN_func.py
    Clean up comments and debugging code..
    Very minor comment changes to ISBN_validator.py and README file.

--- ISBN_func.py
+++ ISBN_func.py
@@ -7,16 +7,14 @@ r/DailyProgrammer challenge #197:easy
 
 def isbn_validate(isbn):
 	"""Takes an ISBN (MUST BE STRING) as argument and returns True or False."""
-	isbn = str(isbn) #To stop crashing on non-strings
+	isbn = str(isbn) #To stop function crashing on non-strings
 	isbn = isbn.replace("-","") #remove -
 	import re
 	type10 = re.compile(r"^\d{10}$") # 10 digit
 	type10x = re.compile(r"^\d{9}X$") # 9 digit with X
 	type13 = re.compile(r"^\d{13}$") # 13 digit
-	print isbn #Debugging
 
 	if type10.match(isbn) or type10x.match(isbn): #10 digit
-		print "10 digit" #Debugging
 		factor = len(isbn)
 		total = 0
 		for digit in isbn:
@@ -25,14 +23,11 @@ def isbn_validate(isbn):
 			total += (int(digit)*factor)
 			factor -= 1
 		if total % 11 == 0 :
-			print "success"	
 			return True
 		else:
-			print "fail"
 			return False
 
 	elif type13.match(isbn): #13 digit
-		print "13 digit" #debug
 		total = 0
 		counter = 1
 		for digit in isbn:
@@ -42,19 +37,11 @@ def isbn_validate(isbn):
 			counter += 1
 			total += digit
 		if total % 10 == 0 :
-			print "success"
 			return True
 		else:
-			print "fail"
 			return False
 	else:  #neither 10 nor 13 digits
-		print "null" #debug
-		print "fail"
 		return False
 #end function
 
-test13 = "978-3-16-148410-0"
-test10x = "0-8044-2957-X"
-test10 = "99921-58-10-7"
-failer = 1.234324
-isbn_validate(failer)
+
--- README.txt
+++ README.txt
@@ -2,7 +2,12 @@ A short python script to validate if a ISBN
 (International Standard Book Number)
  is True or False.
 
-This is for Reddit daily challenge 197:Easy
+This is for Reddit daily programmer challenge 197:Easy
+
+ISBN_validator is a standalone validator.
+
+ISBN_func is a function that will return True or False 
+on a string.
 
 This is my first real project on GitHub, and one of my 
 first pieces of work as a novice programmer.
